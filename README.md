b413fed920d9b570835c3814790612a7b5a28e53
nidhi_experiment <nidhi@narendar-sbis-MacBook-Pro.local>
2023-10-19 15:31:00 +0530

    Resolved conflicts

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
+
 # MathFunctions
 A swift pacakage
+A description of this package.
---
+++ Sources/MathFunctions.xcframework/ios-arm64_x86_64-simulator/MathFunctions.framework/Headers/MathFunctions.h
@@ -0,0 +1,18 @@
+//
+//  MathFunctions.h
+//  MathFunctions
+//
+//  Created by narendar_sbi on 19/10/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MathFunctions.
+FOUNDATION_EXPORT double MathFunctionsVersionNumber;
+
+//! Project version string for MathFunctions.
+FOUNDATION_EXPORT const unsigned char MathFunctionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MathFunctions/PublicHeader.h>
+
+
