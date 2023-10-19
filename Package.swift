14afef77e97d5fb345d58a24e47225abac7abc44
nidhi_experiment <nidhi@narendar-sbis-MacBook-Pro.local>
2023-10-19 17:55:38 +0530

    package file modified

--- Package.swift
+++ Package.swift
@@ -18,6 +18,13 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .binaryTarget(name: "MathFunctions", path: "./Sources/MathFunctions.xcframework")
+//        .target(
+//            name: "MathSwiftPackage",
+//            dependencies: [
+////                .target(name:"MathFunctions"),
+//                .product(name: "LoggerSPM", package: "LoggerSPM"),
+//        ]),
+        .binaryTarget(name: "MathFunctions",
+                      path: "./Sources/MathFunctions.xcframework")
     ]
 )
